d52c93655b03345d17d641c884094a6af8d3ca04
2sejo1 <2sejo1@naver.com>
2024-03-07 17:54:07 +0900

    [수정] 구조 및 스타일 추가

--- work/siita/js/front.js
+++ work/siita/js/front.js
@@ -2,10 +2,9 @@ $(document).ready(function () {
   //press
   const pressSwiper = new Swiper(".press-swiper", {
     // 추가 옵션 설정
-    slidesPerView: 3, // 한 화면에 나올 슬라이드 개수
-    spaceBetween: 10, // 슬라이드 사이 간격 px
+    slidesPerView: "auto", // 한 화면에 나올 슬라이드 개수
     loop: false, // 무한 반복 여부
-    centeredSlides: false, //활성 슬라이드를 항상 가운데에 배치
+    centeredSlides: true, //활성 슬라이드를 항상 가운데에 배치
 
     // Responsive breakpoints 반응형 분기
     // breakpoints: {
@@ -24,10 +23,18 @@ $(document).ready(function () {
     // },
 
     // Navigation arrows 좌/우 컨트롤 버튼
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
   });
+
+  document.querySelector(".prev-btn").addEventListener("click", function () {
+    pressSwiper.slidePrev();
+  });
+  document.querySelector(".next-btn").addEventListener("click", function () {
+    pressSwiper.slideNext();
+  });
+
   // //press
 });
