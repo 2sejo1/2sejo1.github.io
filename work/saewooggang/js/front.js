57898092de5c5d0c337fd430f88fbb45dc14bd3c
2sejo1 <2sejo1@naver.com>
2024-03-13 18:18:29 +0900

    [수정] 구조 및 스타일, 동작 추가

--- work/saewooggang/js/front.js
+++ work/saewooggang/js/front.js
@@ -42,15 +42,6 @@ $(document).ready(function () {
   // // sns - post slide (main visual sect)
 
   // side btn img: on-off
-  // document
-  //   .querySelectorAll(".content-box .btn-list a")
-  //   .forEach(function (element) {
-  //     element.addEventListener("mouseenter", function () {
-  //       let getSrc = this.children[0].src;
-  //       let newSrc = getSrc.replace("Off.png", "On.png");
-  //       getSrc = newSrc;
-  //     });
-  //   });
   document
     .querySelectorAll(".content-box .btn-list a")
     .forEach(function (element) {
@@ -66,5 +57,62 @@ $(document).ready(function () {
       });
     });
 
+  // 이하 동일 동작 코드
+
+  // function toggleImg(imgElem, hover, leave) {
+  //   let changImg = $(imgElem).attr("src").replace(hover, leave);
+  //   console.log(changImg);
+  //   $(imgElem).attr("src", changImg);
+  // }
+  // $(".secMenu ul li a").on({
+  //   mouseenter: function () {
+  //     let thisImg = $(this).find("img");
+  //     console.log(thisImg);
+  //     toggleImg($(thisImg), "Off", "On");
+  //   },
+  //   mouseleave: function () {
+  //     let thisImg = $(this).find("img");
+  //     console.log(thisImg);
+  //     toggleImg($(thisImg), "On", "Off");
+  //   },
+  // });
+
   // // side btn img: on-off
+
+  //  side btn click : content open
+  // document.querySelectorAll(".content-list .content").forEach(function (e) {
+  //   let contentH = e.clientHeight;
+  //   console.log(contentH);
+  // });
+
+  document
+    .querySelectorAll(".content-box .btn-list li a")
+    .forEach(function (e) {
+      e.addEventListener("click", function () {
+        let contentID = this.getAttribute("href");
+        let contentElement = document.getElementById(contentID.slice(1));
+        let contentH = contentElement.clientHeight;
+
+        contentElement.parentElement.style.maxHeight = contentH + 10 + "px";
+      });
+    });
+
+  // // side btn click : content open
+
+  //  close btn click : content close
+  document
+    .querySelector(".content-list")
+    .addEventListener("click", function (e) {
+      if (e.target.classList.contains("close-btn")) {
+        // close-btn을 클릭했을 때만 처리 (이벤트 위임)
+        var closestLi = e.target.closest("li");
+        // 현재 close-btn이 속한 가장 가까운 li 요소 찾기
+        if (closestLi) {
+          // 가장 가까운 li 요소가 있으면 해당 li 요소의 max-height를 0으로 설정
+          closestLi.style.maxHeight = "0";
+        }
+      }
+    });
+
+  // // close btn click : content close
 });
