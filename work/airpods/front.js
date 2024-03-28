0f2d468afccea6558d340697652384094666de69
2sejo1 <2sejo1@naver.com>
2024-03-28 18:11:10 +0900

    git test

--- work/airpods/front.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".product-box .arrow-btn").on("click", function () {
-    $(this).parent().parent().toggleClass("on");
-    $(this).toggleClass("on");
-  });
-});
