387b6820800c8f399267534a1ca9ea90eb03b058
2sejo1 <2sejo1@naver.com>
2024-06-26 21:07:16 +0900

    [추가] 섹션3 완료

--- work/cheongwadae/js/index.js
+++ work/cheongwadae/js/index.js
@@ -1,56 +1,56 @@
-// let head = "./html/header.html";
-// let foot = "./html/footer.html";
+let head = "./html/header.html";
+let foot = "./html/footer.html";
 
-// let header = document.querySelector("header");
-// let footer = document.querySelector("footer");
+let header = document.querySelector("header");
+let footer = document.querySelector("footer");
 
-// async function fetchContent(place, url) {
-//   try {
-//     const res = await fetch(url);
-//     const data = await res.text();
-//     place.innerHTML = data;
-//   } catch (error) {
-//     console.error("콘텐츠 로딩 중... 오류발생", error);
-//   }
-// }
+async function fetchContent(place, url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.text();
+    place.innerHTML = data;
+  } catch (error) {
+    console.error("콘텐츠 로딩 중... 오류발생", error);
+  }
+}
 
-// fetchContent(header, head);
-// fetchContent(footer, foot);
+fetchContent(header, head);
+fetchContent(footer, foot);
 
-document.addEventListener("DOMContentLoaded", function () {
-  // header.html을 로드하여 header 요소에 삽입
-  fetch("./html/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("header").innerHTML = data;
-      // css
-      const css = document.createElement("link");
-      css.rel = "stylesheet";
-      css.href = "./css/header.css";
-      document.head.appendChild(css);
-      // header.js 스크립트 파일도 로드
-      const script = document.createElement("script");
-      script.src = "./js/header.js";
-      document.body.appendChild(script);
-    })
-    .catch((error) => console.error("Error loading header:", error));
+// document.addEventListener("DOMContentLoaded", function () {
+//   // header.html을 로드하여 header 요소에 삽입
+//   fetch("./html/header.html")
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.querySelector("header").innerHTML = data;
+//       // css
+//       const css = document.createElement("link");
+//       css.rel = "stylesheet";
+//       css.href = "./css/header.css";
+//       document.head.appendChild(css);
+//       // header.js 스크립트 파일도 로드
+//       const script = document.createElement("script");
+//       script.src = "./js/header.js";
+//       document.body.appendChild(script);
+//     })
+//     .catch((error) => console.error("Error loading header:", error));
 
-  // footer.html을 로드하여 footer 요소에 삽입
-  fetch("./html/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("footer").innerHTML = data;
+//   // footer.html을 로드하여 footer 요소에 삽입
+//   fetch("./html/footer.html")
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.querySelector("footer").innerHTML = data;
 
-      //css
-      // css
-      const css = document.createElement("link");
-      css.rel = "stylesheet";
-      css.href = "./css/footer.css";
-      document.head.appendChild(css);
-    })
+//       //css
+//       // css
+//       const css = document.createElement("link");
+//       css.rel = "stylesheet";
+//       css.href = "./css/footer.css";
+//       document.head.appendChild(css);
+//     })
 
-    .catch((error) => console.error("Error loading footer:", error));
-});
+//     .catch((error) => console.error("Error loading footer:", error));
+// });
 
 // main visual
 
@@ -121,3 +121,12 @@ const contentSwiper = new Swiper(".sect2 .content-swiper", {
   },
 });
 // //sect2
+
+// sect3
+document.querySelectorAll(".sect3 .mn-list li").forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    document.querySelector(".sect3 .mn-list .on").classList.remove("on");
+    e.classList.add("on");
+  });
+});
+// //sect3
